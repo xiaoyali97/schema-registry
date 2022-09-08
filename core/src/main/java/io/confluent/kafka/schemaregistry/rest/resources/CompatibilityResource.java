c7b344255897e472817899ab43f0aee78f0b9370
Xiaoya Li <xli@confluent.io>
2022-09-08 12:36:33 -0700

    DGS-3396: Add deletedOnly=true to SR list APIs

--- core/src/main/java/io/confluent/kafka/schemaregistry/rest/resources/CompatibilityResource.java
+++ core/src/main/java/io/confluent/kafka/schemaregistry/rest/resources/CompatibilityResource.java
@@ -26,6 +26,7 @@ import io.confluent.kafka.schemaregistry.exceptions.SchemaRegistryStoreException
 import io.confluent.kafka.schemaregistry.rest.VersionId;
 import io.confluent.kafka.schemaregistry.rest.exceptions.Errors;
 import io.confluent.kafka.schemaregistry.storage.KafkaSchemaRegistry;
+import io.confluent.kafka.schemaregistry.storage.LookupFilter;
 import io.confluent.kafka.schemaregistry.utils.QualifiedSubject;
 import io.confluent.rest.annotations.PerformanceMetric;
 import io.swagger.v3.oas.annotations.Operation;
@@ -197,7 +198,7 @@ public class CompatibilityResource {
     List<Schema> previousSchemas = new ArrayList<>();
     try {
       //Don't check compatibility against deleted schema
-      schemaRegistry.getAllVersions(subject, false)
+      schemaRegistry.getAllVersions(subject, LookupFilter.DEFAULT)
           .forEachRemaining(previousSchemas::add);
     } catch (SchemaRegistryException e) {
       throw Errors.storeException("Error while retrieving schema for subject "
